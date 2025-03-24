d2d237a24c3e2b45c9a85b3f75abbcfb815ed971
SalamJean <salamjeanlouis8@gmail.com>
2025-03-24 17:42:14 +0000

    fix: readme exo

--- axios-api.js
+++ axios-api.js
@@ -3,18 +3,18 @@ const PLACEHOLDER_URL_API = "https://jsonplaceholder.typicode.com/users";
 const userID = 1;
 
 // Axios ALL
-// axios.get(PLACEHOLDER_URL_API)
-//   .then(response => {
-//     console.log(response.data);
-//   })
-//   .catch(error => console.error("Erreur GET ALL:", error.message));
+axios.get(PLACEHOLDER_URL_API)
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => console.error("Erreur GET ALL:", error.message));
 
 // Axios by ID
-// axios.get(`${PLACEHOLDER_URL_API}/${userID}`)
-//   .then(response => {
-//     console.log(response.data);
-//   })
-//   .catch(error => console.error("Erreur GET by ID:", error.message));
+axios.get(`${PLACEHOLDER_URL_API}/${userID}`)
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => console.error("Erreur GET by ID:", error.message));
 
 // axios by POST
 axios.post(PLACEHOLDER_URL_API, {
@@ -32,23 +32,23 @@ axios.post(PLACEHOLDER_URL_API, {
   .catch(error => console.error("Erreur POST:", error.message));
 
 // fetch by PUT
-// axios.put(`${PLACEHOLDER_URL_API}/${userID}`, {
-//     name: "Amon Marcel",
-//     username: "amon",
-//     email: "amon@gmail.com"
-//   }, {
-//     headers: {
-//       'Content-type': 'application/json; charset=UTF-8',
-//     }
-//   })
-//   .then(response => {
-//     console.log(response.data);
-//   })
-//   .catch(error => console.error("Erreur PUT:", error.message));
+axios.put(`${PLACEHOLDER_URL_API}/${userID}`, {
+    name: "Amon Marcel",
+    username: "amon",
+    email: "amon@gmail.com"
+  }, {
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    }
+  })
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => console.error("Erreur PUT:", error.message));
 
 // fetch by PUT
-// axios.delete(`${PLACEHOLDER_URL_API}/${userID}`)
-//   .then(response => {
-//     console.log(response.data);
-//   })
-//   .catch(error => console.error("Erreur DELETE:", error.message));
+axios.delete(`${PLACEHOLDER_URL_API}/${userID}`)
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => console.error("Erreur DELETE:", error.message));
